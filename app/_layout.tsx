86854d69556328c25be3af6defa3cc8109d8b4f0
Amr Tamer Elharery <amrelharery@gmail.com>
2026-02-03 03:44:11 +0200

    Add globals in root component

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,11 @@
-import { Stack } from "expo-router";
+import { ThemeProvider } from '@/lib/theme-context';
+import { Stack } from 'expo-router';
+import './global.css';
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <ThemeProvider defaultTheme="system">
+      <Stack />
+    </ThemeProvider>
+  );
 }
